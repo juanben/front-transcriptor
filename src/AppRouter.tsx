dfbf1bd16ef456000c73b21601ac9c8f7d55e214
Juan <juan.benavides@epn.edu.ec>
2026-03-31 18:02:35 -0500

    Pantalla about

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './components/logIn/Login';
 import SignUp from './components/signUp/SignUp';
-
+import About from './components/about/About';
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="/testRec" element={<App />} />
+             
       </Routes>
     </Router>
   );
